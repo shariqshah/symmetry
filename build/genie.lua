f121fc75a4a2727b8fd83be3fdd6111d9fba714c
shariq <bluerriq@gmail.com>
2017-08-23 02:05:52 +0500

    Fixed minor error in genie.lua

--- build/genie.lua
+++ build/genie.lua
@@ -6,21 +6,25 @@ defines {"USE_GLAD"}
 includedirs {"../include/"}
 buildoptions {"-Wall", "-std=c99"}
 if os.is("linux") then
-   postbuildcommands {"ln -fs /mnt/Dev/Projects/symmetry/assets debug"}
-   postbuildcommands {"ln -fs /mnt/Dev/Projects/symmetry/assets release"}
+   postbuildcommands {"ln -fs /mnt/Dev/Projects/symmetry/assets debug/"}
+   postbuildcommands {"ln -fs /mnt/Dev/Projects/symmetry/assets release/"}
 end
 
 configuration "Debug"
-if not _ACTION == nil then
+if _ACTION ~= nil then
    os.mkdir(_ACTION .. "/debug")
    targetdir (_ACTION .. "/debug")
 end
+defines { "DEBUG" }
+flags { "Symbols" }
 
 configuration "Release"
-if not _ACTION == nil then
+if _ACTION ~= nil then
    os.mkdir(_ACTION .. "/release")
    targetdir (_ACTION .. "/release")
 end
+defines { "NDEBUG" }
+flags { "OptimizeSpeed"}
 
 -------------------------
 -- Game
@@ -37,14 +41,6 @@ buildoptions {"`pkg-config --cflags-only-other sdl2 openal`"}
 linkoptions {"`pkg-config --libs sdl2 openal`"}
 links {"m"}
 
-configuration "Debug"
-defines { "DEBUG" }
-flags { "Symbols" }
-
-configuration "Release"
-defines { "NDEBUG" }
-flags { "OptimizeSpeed"}
-
 -------------------------
 -- libSymmetry
 -------------------------
@@ -57,8 +53,3 @@ files { "../src/common/**.c", "../src/libsymmetry/**.c" }
 
 configuration "Debug"
 defines {"GL_DEBUG_CONTEXT", "AL_DEBUG"}
-flags {"Symbols"}
-
-configuration "Release"
-defines { "NDEBUG" }
-flags { "OptimizeSpeed"}

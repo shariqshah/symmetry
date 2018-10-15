785b762c7ab63c70d49e5721962029d6c6912c09
Shariq Shah <bluerriq@gmail.com>
2018-10-15 22:00:07 +1100

    Moved genie and other tools to tools folder

--- tools/setup_env.bat
+++ tools/setup_env.bat
@@ -3,6 +3,6 @@
 call D:\VS_2017\VC\Auxiliary\Build\vcvarsall.bat x64
 cls
 set edito="C:\Program Files\emacs-26.1-x86_64\bin\runemacs.exe"
-set path=W:\misc;%path% rem 
+set path=W:\tools;%path% rem 
 
 
